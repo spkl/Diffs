c01dffbfdf14e1e18d08cfde773abdccdcb79722
spkl <spkl>
2021-01-31 18:29:24 +0100

    - added unit tests - fixed issue of comparer not being used

---
+++ test/MyersDiffClass.ReferenceCase.cs
@@ -0,0 +1,37 @@
+﻿namespace spkl.Diffs.Test
+{
+    public partial class MyersDiffClass
+    {
+        public class ReferenceCase
+        {
+            public string[] A { get; set; }
+
+            public string AString
+            {
+                set
+                {
+                    this.A = value.Split(',');
+                }
+            }
+
+            public string[] B { get; set; }
+
+            public string BString
+            {
+                set
+                {
+                    this.B = value.Split(',');
+                }
+            }
+
+            public (ResultType ResultType, string AItem, string BItem)[] Result { get; set; }
+
+            public (int LineA, int LineB, int CountA, int CountB)[] EditScript { get; set; }
+
+            public override string ToString()
+            {
+                return $"{string.Join(",", this.A)} / {string.Join(",", this.B)}";
+            }
+        }
+    }
+}
---
+++ test/MyersDiffClass.cs
@@ -0,0 +1,67 @@
+using NUnit.Framework;
+using System;
+using System.Linq;
+
+namespace spkl.Diffs.Test
+{
+    public partial class MyersDiffClass
+    {
+        [Test]
+        public void InputValidation()
+        {
+            Assert.That(() => new MyersDiff<string>(null, new string[] { "" }), Throws.ArgumentNullException);
+            Assert.That(() => new MyersDiff<string>(new string[] { "" }, null), Throws.ArgumentNullException);
+            Assert.That(() => new MyersDiff<string>(new string[0], new string[0]), Throws.Nothing);
+        }
+
+        [Test]
+        public void CustomComparer()
+        {
+            string[] a = new[] { "a", "b", "c", "a", "b", "b", "a" };
+            string[] b = new[] { "C", "B", "A", "B", "A", "C" };
+
+            MyersDiff<string> diff = new MyersDiff<string>(a, b, StringComparer.OrdinalIgnoreCase);
+
+            (ResultType, string, string)[] result = new[]
+            {
+                (ResultType.A, "a", null),
+                (ResultType.B, null, "C"),
+                (ResultType.Both, "b", "B"),
+                (ResultType.A, "c", null),
+                (ResultType.Both, "a", "A"),
+                (ResultType.Both, "b", "B"),
+                (ResultType.A, "b", null),
+                (ResultType.Both, "a", "A"),
+                (ResultType.B, null, "C"),
+            };
+
+            (int, int, int, int)[] editScript = new[]
+            {
+                    (0, 0, 1, 1),
+                    (2, 2, 1, 0),
+                    (5, 4, 1, 0),
+                    (7, 5, 0, 1)
+            };
+
+            Assert.Multiple(() =>
+            {
+                Assert.That(diff.GetResult().ToArray(), Is.EqualTo(result));
+                Assert.That(diff.GetEditScript().ToArray(), Is.EqualTo(editScript));
+            });
+        }
+
+        [Test]
+        [TestCaseSource(nameof(ReferenceCases))]
+        public void ReferenceResult(ReferenceCase testCase)
+        {
+            Assert.That(new MyersDiff<string>(testCase.A, testCase.B).GetResult().ToArray(), Is.EqualTo(testCase.Result));
+        }
+
+        [Test]
+        [TestCaseSource(nameof(ReferenceCases))]
+        public void ReferenceEditScript(ReferenceCase testCase)
+        {
+            Assert.That(new MyersDiff<string>(testCase.A, testCase.B).GetEditScript().ToArray(), Is.EqualTo(testCase.EditScript));
+        }
+    }
+}
